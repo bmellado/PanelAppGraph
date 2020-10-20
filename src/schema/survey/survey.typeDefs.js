32da2438d3456e8e01e5d5fccbc562dcff50c4b7
Valentina <vrojas5@uc.cl>
2020-10-19 22:54:36 -0300

    chore: add survey structure for admin

--- src/schema/survey/survey.typeDefs.js
+++ src/schema/survey/survey.typeDefs.js
@@ -18,11 +18,22 @@ const typeDefs = gql`
   extend type Mutation {
     editOrCreateSurvey(input: EditOrCreateSurveyInput!): Survey!
     deleteSurvey(surveyId: ID!): Boolean!
+    createSurvey(
+      cycle: ID
+      description: String!
+      name: String!
+      panel: ID
+      pointsAmount: Int!
+      tags: [Tag]
+      qualtricsName: String!
+      qualtricsId: String!
+    ): Survey!
   }
 
   extend type Query {
+    Survey:(id: ID!): Survey
     panelSurveys(panelId: ID!): [Survey]
-    surveys: [Survey]
+    allSurveys: [Survey]
   }
 
   input EditOrCreateSurveyInput {
